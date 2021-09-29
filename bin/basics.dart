95a744a2b8aa3c72ed65c5beb07b6577ec426aae
Bilal Ra'ad <bilal52040@gmail.com>
2021-09-29 12:05:17 +0300

    adding some lines from the lecture

--- bin/basics.dart
+++ bin/basics.dart
@@ -1,12 +1,30 @@
 void main() {
   int one = 1;
-  double onePointOne = 1;
+  double onePointOne = 1.1;
   String oneString = 'one';
   bool iLoveCats = true;
+
+  print(oneString);
+  print('this is numbrt one: $one');
+
   //0 , 1, 2
   List cats = ["cat 1", "cat 2", "cat 3"];
   List<String> catsTyped = ["cat 1", "cat 2", "cat 3"];
-  List<int> numbers = [1, 2, 3];
+  List<int> numbers = [1, 2, 3, 4];
+  List<int> moreNumbers = [1, 2, 3, 4];
+  Map<String, dynamic> myMap = {
+    'name': 'Bilal',
+    'age': 20,
+    'name': 20,
+  };
+
+  print(myMap['name']);
+  for (var number in moreNumbers) {
+    numbers.add(number);
+  }
+  moreNumbers.forEach((number) {
+    numbers.add(number);
+  });
 
   //####### for loop
   //1
